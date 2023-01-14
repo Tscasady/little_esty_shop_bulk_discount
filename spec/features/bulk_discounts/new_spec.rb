eb0b99ef0133056c26d95095732b25e850576655
Thomas Casady <tscasady@gmail.com>
2023-01-14 16:36:48 -0600

    test: Add sad path testing to new bulk page.

--- spec/features/bulk_discounts/new_spec.rb
+++ spec/features/bulk_discounts/new_spec.rb
@@ -26,37 +26,54 @@ RSpec.describe 'The Bulk Discount new page', type: :feature do
       expect(current_path).to eq merchant_bulk_discounts_path(merchant_1)
     end
 
-    describe 'it returns to the new page with an error message' do
+    describe 'it returns to the new page with an error message if invalid data is given' do
       context 'if discount < 0' do
-        it "displays ''" do
+        it "displays discountmust be greater than 0" do
           fill_in "Discount", with: -2
+          fill_in "Threshold", with: 10
           click_button "Submit"
-          expect(current_path).to eq merchant_bulk_discounts_path(merchant_1)
-          expect(page).to have_content ""
+          expect(current_path).to eq new_merchant_bulk_discount_path(merchant_1)
+          expect(page).to have_content "Discount must be greater than 0"
         end
       end
 
-      context 'if discount > 100' do
-        it '' do
-
+      context 'if discount < 100' do
+        it 'displays discount must be less than 100' do
+          fill_in "Discount", with: 110 
+          fill_in "Threshold", with: 10
+          click_button "Submit"
+          expect(current_path).to eq new_merchant_bulk_discount_path(merchant_1)
+          expect(page).to have_content "Discount must be less than 100"
         end
       end
 
       context 'if discount is a string' do
-        it '' do
-
+        it 'displays that discount is not a number' do
+          fill_in "Discount", with: "Big Discount"
+          fill_in "Threshold", with: 10
+          click_button "Submit"
+          expect(current_path).to eq new_merchant_bulk_discount_path(merchant_1)
+          expect(page).to have_content "Discount is not a number"
         end
       end
 
       context 'if threshold is < 0' do
-        it '' do
-
+        it 'displays threshold must be greater than 0' do
+          fill_in "Discount", with: 20
+          fill_in "Threshold", with: -10
+          click_button "Submit"
+          expect(current_path).to eq new_merchant_bulk_discount_path(merchant_1)
+          expect(page).to have_content "Threshold must be greater than 0"
         end
       end
 
       context 'if threshold is a string' do
-        it '' do
-
+        it 'displays threshold is not a number' do
+          fill_in "Discount", with: 20
+          fill_in "Threshold", with: "A few items"
+          click_button "Submit"
+          expect(current_path).to eq new_merchant_bulk_discount_path(merchant_1)
+          expect(page).to have_content "Threshold is not a number"
         end
       end
     end
