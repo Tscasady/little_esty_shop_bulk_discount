d04d8f400a18e2c5b67653398b9b8fc0c11f31ce
Thomas Casady <tscasady@gmail.com>
2023-01-14 13:54:20 -0600

    test: Add test for create discount button in bulk index page.

--- spec/features/bulk_discounts/index_spec.rb
+++ spec/features/bulk_discounts/index_spec.rb
@@ -8,8 +8,10 @@ RSpec.describe 'The bulk discount index page', type: :feature do
     let!(:bulk_discount_2) { create(:bulk_discount, merchant: merchant_1, discount: 30, threshold: 15) }
     let!(:bulk_discount_3) { create(:bulk_discount, merchant: merchant_1, discount: 10, threshold: 5) }
 
-    it 'displays all discounts with their percentage and thresholds' do
+    before(:each) do
       visit merchant_bulk_discounts_path(merchant_1)
+    end
+    it 'displays all discounts with their percentage and thresholds' do
 
       expect(page).to have_content "Discount: #{merchant_1.bulk_discounts.first.discount}"      
       expect(page).to have_content "Discount: #{merchant_1.bulk_discounts.second.discount}"      
@@ -20,8 +22,15 @@ RSpec.describe 'The bulk discount index page', type: :feature do
     end
 
     it 'has a link to each discount show page' do
-      visit merchant_bulk_discounts_path(merchant_1)
       expect(page).to have_link "Discount Info", count: merchant_1.bulk_discounts.length
+    end
+
+    it 'has a link to create a new discount' do
+      expect(page).to have_button "Create Discount"
+
+      click_button "Create Discount"
+
+      expect(current_path).to eq new_merchant_bulk_discount
     end
   end
 end
