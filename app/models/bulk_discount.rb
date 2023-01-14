a51cf43a2c2513d474dbae4a280c500b26dc161d
Thomas Casady <tscasady@gmail.com>
2023-01-14 16:21:02 -0600

    feat: Add bulk model validations and validation tests.

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,3 +1,7 @@
 class BulkDiscount < ApplicationRecord
   belongs_to :merchant
+
+  validates_numericality_of :discount, greater_than: 0
+  validates_numericality_of :discount, less_than: 100
+  validates_numericality_of :threshold, greater_than: 0
 end
