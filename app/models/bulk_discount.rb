3c84f7d024c7ec56f6bfdcb1f89f8ab8b55cde4a
Thomas Casady <tscasady@gmail.com>
2023-01-15 13:35:54 -0600

    feat: Add bulk_discount merchant, items, and ii relations.

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,5 +1,7 @@
 class BulkDiscount < ApplicationRecord
   belongs_to :merchant
+  has_many :items, through: :merchant
+  has_many :invoice_items, through: :items
 
   validates_numericality_of :discount, greater_than: 0, less_than: 100
   validates_numericality_of :threshold, greater_than: 0
