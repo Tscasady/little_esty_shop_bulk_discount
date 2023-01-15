5e8715db8467a863b75222d4cc2b2f0293f0c3f3
Thomas Casady <tscasady@gmail.com>
2023-01-14 18:51:39 -0600

    test: Add test for edit link on show page.

--- spec/features/bulk_discounts/show_spec.rb
+++ spec/features/bulk_discounts/show_spec.rb
@@ -11,9 +11,14 @@ RSpec.describe 'The Bulk Discount Show page', type: :feature do
     end
 
     it 'displays threshold and percent discount of the bulk discount' do
-
       expect(page).to have_content "Discount: #{bd_1.discount}%"
       expect(page).to have_content "Threshold: #{bd_1.threshold}"
+    end
+
+    it 'has a link to edit the bulk discount and it redirects to the edit page' do
+      expect(page).to have_link "Edit Discount"
+      click_link "Edit Discount"
+      expect(current_path).to eq edit_merchant_bulk_discount_path(merchant_1, bd_1)
     end
   end
 end
