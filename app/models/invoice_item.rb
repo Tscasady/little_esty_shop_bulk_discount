3b3ca9dc4f8c8f3764991a9229a1942d5af52e37
Thomas Casady <tscasady@gmail.com>
2023-01-16 13:13:47 -0600

    feat: Add discount method to invoice_item.

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -15,4 +15,8 @@ class InvoiceItem < ApplicationRecord
     invoice_ids = InvoiceItem.where("status = 0 OR status = 1").pluck(:invoice_id)
     Invoice.order(created_at: :asc).find(invoice_ids)
   end
+
+  def discount
+    self.bulk_discounts.joins(:invoice_items).where("invoice_items.id = ?", self.id).where("bulk_discounts.threshold <= invoice_items.quantity").maximum("bulk_discounts.discount")
+  end
 end
