9a2c711649f8ccbf36970bceb505b51cd141ad3b
Thomas Casady <tscasady@gmail.com>
2023-01-16 13:09:46 -0600

    test: Add test for invoice_item.discount method.

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -31,13 +31,26 @@ RSpec.describe InvoiceItem, type: :model do
       @i3 = Invoice.create!(customer_id: @c2.id, status: 2)
       @i4 = Invoice.create!(customer_id: @c3.id, status: 2)
       @i5 = Invoice.create!(customer_id: @c4.id, status: 2)
-      @ii_1 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_1.id, quantity: 1, unit_price: 10, status: 0)
-      @ii_2 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_2.id, quantity: 1, unit_price: 8, status: 0)
+      @ii_1 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_1.id, quantity: 10, unit_price: 10, status: 0)
+      @ii_2 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_2.id, quantity: 20, unit_price: 8, status: 0)
       @ii_3 = InvoiceItem.create!(invoice_id: @i2.id, item_id: @item_3.id, quantity: 1, unit_price: 5, status: 2)
       @ii_4 = InvoiceItem.create!(invoice_id: @i3.id, item_id: @item_3.id, quantity: 1, unit_price: 5, status: 1)
+
+      @bd_1 = BulkDiscount.create!(threshold: 10, discount: 20, merchant: @m1)
+      @bd_2 = BulkDiscount.create!(threshold: 20, discount: 30, merchant: @m1)
     end
     it 'incomplete_invoices' do
       expect(InvoiceItem.incomplete_invoices).to eq([@i1, @i3])
+    end
+  end
+
+  describe 'instance methods' do
+    describe 'discount' do
+      it 'returns the discount that has been applied' do
+        expect(@ii_1.discount).to eq 20
+        expect(@ii_2.discount).to eq 30
+        expect(@ii_3.discount).to eq nil
+      end
     end
   end
 end
