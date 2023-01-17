62db7061b9311e8ea385c7ca7cad5bb607dfd39c
Thomas Casady <tscasady@gmail.com>
2023-01-16 21:53:06 -0600

    feat: Validate presence of name.

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -3,6 +3,7 @@ class BulkDiscount < ApplicationRecord
   has_many :items, through: :merchant
   has_many :invoice_items, through: :items
 
+  validates_presence_of :name
   validates_numericality_of :discount, greater_than: 0, less_than: 100
   validates_numericality_of :threshold, greater_than: 0
 end
